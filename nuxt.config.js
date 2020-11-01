19c71d20a792f05f86b0aa2edede15e428b262da
devDohyun <dohyun10101@gmail.com>
2020-11-02 00:01:08 +0900

    Added env & nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,21 @@ require('dotenv').config({path: '.env'})
 
 export default {
     server: {
-        host: '0.0.0.0',
-        port: 3000
+        host: process.env.SERVER_HOST || 'localhost',
+        port: process.env.SERVER_PORT || 80
+    },
+    env: {
+        NAME: process.env.NAME,
+        HEADER_TITLE: process.env.HEADER_TITLE
+    },
+
+    globalName: 'DotubeMusic',
+    head: {
+        titleTemplate: '%s - ' + process.env.HEADER_TITLE,
+        title: process.env.HEADER_TITLE || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        ]
     }
 }
