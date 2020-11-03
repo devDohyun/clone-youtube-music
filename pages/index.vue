1b33613c73b95cc75c02293c8063e46867ffcf6e
devDohyun <dohyun10101@gmail.com>
2020-11-04 01:45:06 +0900

    Modified text global : middle dot

--- pages/index.vue
+++ pages/index.vue
@@ -49,28 +49,28 @@ export default {
                             id: 0,
                             type: 'music',
                             title: '흔들리는 꽃들 속에서 네 샴푸향이 느껴진거야',
-                            desc: '노래ㆍ장범준',
+                            desc: '노래•장범준',
                             thumb: 'https://storage.googleapis.com/clone-youtube-music/images/album/흔들리는-꽃들-속에서-네-샴푸향이-느껴진거야.jpg'
                         },
                         {
                             id: 1,
                             type: 'music',
                             title: '스토커',
-                            desc: '노래ㆍ10cm(십센치)',
+                            desc: '노래•10cm(십센치)',
                             thumb: 'https://storage.googleapis.com/clone-youtube-music/images/album/스토커.jpg'
                         },
                         {
                             id: 2,
                             type: 'music',
                             title: '길',
-                            desc: '노래ㆍ버스커버스커',
+                            desc: '노래•버스커버스커',
                             thumb: 'https://storage.googleapis.com/clone-youtube-music/images/album/684015_1_f.jpg'
                         },
                         {
                             id: 23,
                             type: 'mix',
                             title: '속이 뻥 뚫리는 역대급 가창력',
-                            desc: '재생목록ㆍYouTube Music',
+                            desc: '재생목록•YouTube Music',
                             thumb: ''
                         },
                     ]
