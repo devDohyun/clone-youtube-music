3b87016e576bb66759cf7217481d06dc90aa7e69
devDohyun <dohyun10101@gmail.com>
2020-11-04 01:38:27 +0900

    Different template : Type - mix, music

--- components/Album/Item.vue
+++ components/Album/Item.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="album-item">
+    <div
+        @click="handleItemClick"
+        class="album-item"
+    >
         <div
             :style="{'background-image': `url(${albumThumb})`}"
             class="album-thumb"
         >
-            <div class="album-overlay">
-                <button @click="handleAlbumClick" type="button" class="btn-play">
+            <template v-if="albumType === 'music'">
+                <button @click="handlePlayClick" type="button" class="btn-play-music">
                     <fa-icon :icon="['fas', 'play']"></fa-icon>
                 </button>
+            </template>
+            <div class="album-overlay">
+                <template v-if="albumType === 'mix'">
+                    <button @click.stop="handlePlayClick" type="button" class="btn-play-mix">
+                        <fa-icon :icon="['fas', 'play']"></fa-icon>
+                    </button>
+                </template>
             </div>
         </div>
         <div class="album-title">{{ albumTitle }}</div>
@@ -64,6 +74,29 @@
 
             color: $color_gray3;
         }
+        .btn-play-music {
+            z-index: 1;
+
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+
+            display: block;
+            width: 35px;
+            height: 35px;
+            padding: 0;
+
+            border: none;
+            outline: none;
+
+            background: transparent;
+            color: white;
+
+            font-size: 24px;
+
+            cursor: pointer;
+        }
         .album-overlay {
             position: absolute;
 
@@ -75,7 +108,7 @@
 
             transition: opacity 0.2s ease;
 
-            .btn-play {
+            .btn-play-mix {
                 position: absolute;
                 right: 17.5px;
                 bottom: 17.5px;
@@ -113,27 +146,44 @@ export default {
     data () {
         return {
             albumId: this.id,
+            albumType: this.type,
             albumThumb: this.thumb,
             albumTitle: this.title,
             albumDesc: this.desc
         }
     },
     props: {
-        id: {},
+        id: {
+            type: [String, Number],
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
         thumb: {},
         title: {
+            type: String,
             required: true
         },
         desc: {}
     },
     methods: {
-        async handleAlbumClick () {
+        handleItemClick () {
+            if (this.albumType === 'music') {
+                this.handlePlayClick()
+            } else if (this.albumType === 'mix') {
+                this.$router.push('/explorer')
+            }
+        },
+        async handlePlayClick () {
             const index = await this.addPlaylist()
             this.$store.commit('player/setPlayIndex', index)
         },
         async addPlaylist () {
             const payload = {
                 id: this.id,
+                type: this.type,
                 thumb: this.thumb,
                 title: this.title,
                 desc: this.desc
