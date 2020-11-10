4c35d9c289b628407b89b34f0f4cbbade0adb750
unknown <dohyun10101@gmail.com>
2020-11-10 21:54:15 +0900

    Added player interval

--- store/player.js
+++ store/player.js
@@ -2,12 +2,15 @@ export const state = () => ({
     isPlaying: false,
     currentSeconds: 0,
     playIndex: null,
-    playlist: []
+    playlist: [],
+
+    playerIntervalId: null
 })
 
 export const getters = {
     getPlayIndex: state => state.playIndex,
     getIsPlaying: state => state.isPlaying,
+    getCurrentSeconds: state => state.currentSeconds,
     getCurrentMusic: state => state.playlist[state.playIndex],
     getPlaylist: state => state.playlist
 }
@@ -15,7 +18,9 @@ export const getters = {
 export const mutations = {
     addItemInPlaylist: (state, item) => state.playlist.push(item),
     setPlayIndex: (state, index) => state.playIndex = index,
-    setIsPlaying: (state, boolIsPlaying) => state.isPlaying = boolIsPlaying
+    setIsPlaying: (state, boolIsPlaying) => state.isPlaying = boolIsPlaying,
+    setPlayerIntervalId: (state, id) => state.playerIntervalId = id,
+    setCurrentSeconds: (state, seconds) => state.currentSeconds = seconds
 }
 
 export const actions = {
@@ -28,5 +33,13 @@ export const actions = {
         // if (typeof payload === 'string') 
 
         return state.playlist.length - 1
+    },
+    playMusic: ({ commit, state }) => {
+        commit('setIsPlaying', true)
+        const intervalId = setInterval(() => {
+            commit('setCurrentSeconds', state.currentSeconds + 1)
+        }, 1000)
+
+        commit('setPlayerIntervalId', intervalId)
     }
 }
