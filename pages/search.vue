67bbf6e2f66cacde5b9b13f32e28077f3e022175
unknown <dohyun10101@gmail.com>
2020-12-13 18:43:19 +0900

    Added content for no result

--- pages/search.vue
+++ pages/search.vue
@@ -1,20 +1,35 @@
 <template>
     <div id="page-search">
         <div class="page-container">
-            <div class="section-subtitle">노래</div>
-            <div class="section-result">
-                <div
-                    v-for="(song, sIdx) in songsResult"
-                    :key="`${sIdx}${song.title}`"
-                    class="result-item"
-                >
-                    <div :style="{ 'background-image': `url(${song.thumb})`}" class="result-thumb"></div>
-                    <div class="result-text">
-                        <div @click="playSongInPlaylist(song.id)" class="result-title">{{ song.title }}</div>
-                        <div class="result-desc">{{ song.desc }} • {{ $common.formTimeString(song.playtime) }}</div>
+            <template v-if="isResultExist === false">
+                <div class="no-item">
+                    <div class="icon">
+                        <fa-icon :icon="['fas', 'search']"></fa-icon>
+                    </div>
+                    <div class="text-help">
+                        <div class="text-title">검색결과가 없습니다.</div>
+                        다른 검색어를 사용해 보세요.
                     </div>
                 </div>
-            </div>
+            </template>
+            <template v-else>
+                <section v-if="songsResult.legnth > 0">
+                    <div class="section-subtitle">노래</div>
+                    <div class="section-result">
+                        <div
+                            v-for="(song, sIdx) in songsResult"
+                            :key="`${sIdx}${song.title}`"
+                            class="result-item"
+                        >
+                            <div :style="{ 'background-image': `url(${song.thumb})`}" class="result-thumb"></div>
+                            <div class="result-text">
+                                <div @click="playSongInPlaylist(song.id)" class="result-title">{{ song.title }}</div>
+                                <div class="result-desc">{{ song.desc }} • {{ $common.formTimeString(song.playtime) }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </template>
         </div>
     </div>
 </template>
@@ -24,6 +39,26 @@
     }
 
     #page-search {
+        .no-item {
+            text-align: center;
+            color: $color_gray4;
+
+            .icon {
+                margin-bottom: 12px;
+
+                font-size: 96px;
+            }
+
+            .text-help {
+                font-size: 14px;
+
+                .text-title {
+                    margin-bottom: 7.5px;
+                    font-size: 20px;
+                }
+            }
+        }
+        
         .section-result {
             .result-item {
                 display: flex;
@@ -75,6 +110,12 @@ export default {
         }),
         songsResult () {
             return this.getAllsongs.filter(x => x.title.includes(this.query) || x.desc.includes(this.query))
+        },
+        videosResult () {
+            return []
+        },
+        isResultExist () {
+            return !!(this.songsResult.length + this.videosResult.length)
         }
     },
     methods: {
