a7dc7485a85ea26a2e0cc4be644cf71e974f3f36
unknown <dohyun10101@gmail.com>
2020-12-13 17:43:23 +0900

    Added page content

--- pages/search.vue
+++ pages/search.vue
@@ -2,7 +2,19 @@
     <div id="page-search">
         <div class="page-container">
             <div class="section-subtitle">노래</div>
-
+            <div class="section-result">
+                <div
+                    v-for="(song, sIdx) in songsResult"
+                    :key="`${sIdx}${song.title}`"
+                    class="result-item"
+                >
+                    <div :style="{ 'background-image': `url(${song.thumb})`}" class="result-thumb"></div>
+                    <div class="result-text">
+                        <div class="result-title">{{ song.title }}</div>
+                        <div class="result-desc">{{ song.desc }} • {{ $common.formTimeString(song.playtime) }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -10,12 +22,54 @@
     .page-container {
         max-width: 720px;
     }
+
+    #page-search {
+        .section-result {
+            .result-item {
+                display: flex;
+                align-items: center;
+                
+                height: 80px;
+                
+                .result-thumb {
+                    width: 56px;
+                    height: 56px;
+
+                    margin-right: 16px;
+
+                    background-position: center;
+                    background-size: contain;
+                }
+
+                .result-text {
+                    font-size: 14px;
+
+                    .result-title {
+                        margin-bottom: 4px;
+                    }
+                    .result-desc {
+                        color: $color_gray9;
+                    }
+                }
+            }
+        }
+    }
 </style>
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
     data () {
         return {
             query: this.$route.query.q
+        }
+    },
+    computed: {
+        ...mapGetters({
+            getAllsongs: 'api/getAllSongs'
+        }),
+        songsResult () {
+            return this.getAllsongs.filter(x => x.title.includes(this.query) || x.desc.includes(this.query))
         }
     }
 }
