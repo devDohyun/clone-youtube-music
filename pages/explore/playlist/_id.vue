af87e2128012d3955e94127e98d9e8c937609ea2
devDohyun <dohyun10101@gmail.com>
2021-06-07 19:29:14 +0900

    updated page

--- pages/explore/playlist/_id.vue
+++ pages/explore/playlist/_id.vue
@@ -1,112 +1,154 @@
 <template>
-    <div id="page-explore-mix-id">
-        <div class="pannel-mix">
-            <div class="mix-wrapper">
-                <div class="mix-thumb"></div>
-                <div class="mix-info">
-                    <div class="mix-title">{{ title }}</div>
-                    <div class="mix-desc">{{ desc }}</div>
-                    <div class="mix-summary">{{ summary }}</div>
-                    <div class="mix-actions">
-                        <button type="button">셔플</button>
-                        <button type="button">보관함에 추가</button>
-                    </div>
-                </div>
+  <div id="page-explore-mix-id">
+    <template v-if="error">
+      <div class="pannel">
+        {{ error.message }}
+      </div>
+    </template>
+    <template v-else-if="loading">
+      <div class="pannel">
+        <Spinner />
+      </div>
+    </template>
+    <template v-else>
+      <div class="pannel-mix">
+        <div class="mix-wrapper">
+          <div class="mix-thumb" :style="{ 'background-image': `url(${item.thumb})` }"></div>
+          <div class="mix-info">
+            <div class="mix-title">{{ item.title }}</div>
+            <div class="mix-desc">{{ item.desc }}</div>
+            <div class="mix-summary">{{ item.summary }}</div>
+            <div class="mix-actions">
+              <button type="button">셔플</button>
+              <button type="button">보관함에 추가</button>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </template>
+  </div>
 </template>
 <style lang="scss" scoped>
-    #page-explore-mix-id {
-        .pannel-mix {
-            padding: 100px 50px 50px 50px;
-            
-            background-color: $color_gray2;
-            
-            font-size: 0;
+#page-explore-mix-id {
+  .pannel {
+    display: flex;
 
-            .mix-thumb {
-                vertical-align: middle;
+    justify-content: center;
+    align-items: center;
 
-                display: inline-block;
-                width: 240px;
-                height: 240px;
+    width: 100%;
+    height: 100vh;
+  }
+  .pannel-mix {
+    $pannel_left_padding: 50px;
 
-                margin-right: 50px;
+    position: relative;
+    padding: 100px $pannel_left_padding 50px 50px;
 
-                background-color: $color_gray9;
-                border-radius: 5px;
-            }
-            .mix-info {
-                vertical-align: middle;
+    font-size: 0;
 
-                display: inline-block;
-                width: calc(100% - 290px);
-
-                .mix-title {
-                    margin-bottom: 20px;
-                    
-                    font-size: 28px;
-                    font-weight: 700;
-                }
-                .mix-desc, .mix-summary {
-                    font-size: 14px;
-                    color: $color_gray9;
-                }
-            }
-            .mix-actions {
-                margin-top: 50px;
-
-                button {
-                    padding: 7.5px 15px;
-
-                    font-size: 14px;
-                    border: 1px solid white;
-
-                    &:not(:last-of-type) {
-                        margin-right: 15px;
-                    }
-                    &:nth-of-type(1) {
-                        background-color: white;
-                    }
-                    &:nth-of-type(2) {
-                        color: white;
-                    }
-                }
-            }
-        }
+    @media (max-width: $media_md) {
+      padding-bottom: 100px;
     }
+
+    .mix-thumb {
+      vertical-align: middle;
+
+      display: inline-block;
+
+      width: 33%;
+      max-width: 240px;
+
+      margin-right: 50px;
+
+      background-color: $color_gray9;
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+
+      border-radius: 5px;
+
+      &::after {
+        content: ' ';
+        display: block;
+        padding-bottom: 100%;
+      }
+    }
+    .mix-info {
+      vertical-align: middle;
+
+      display: inline-block;
+      width: calc(100% - 290px);
+
+      .mix-title {
+        margin-bottom: 20px;
+
+        font-size: 28px;
+        font-weight: 700;
+      }
+      .mix-desc,
+      .mix-summary {
+        font-size: 14px;
+        color: $color_gray9;
+      }
+      .mix-actions {
+        margin-top: 50px;
+
+        @media (max-width: $media_md) {
+          position: absolute;
+          left: $pannel_left_padding;
+          bottom: $pannel_left_padding / 2;
+        }
+
+        button {
+          padding: 7.5px 15px;
+
+          font-size: 14px;
+          border: 1px solid white;
+
+          &:not(:last-of-type) {
+            margin-right: 15px;
+          }
+          &:nth-of-type(1) {
+            background-color: white;
+          }
+          &:nth-of-type(2) {
+            color: white;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
 <script>
+import Spinner from '@/components/Spinner'
+
 export default {
-    data () {
-        return {
-            title: '',
-            desc: '',
-            summary: ''
-        }
-    },
-    methods: {
-        getMixInfo () {
-            // api logic ...
-
-            return {
-                title: '기타치는 꿀성대 인디/어쿠스틱',
-                desc: '재생목록 • YouTube Music • 2020',
-                summary: '노래 33곡 • 1시간 59분'
-            }
-        },
-
-        dispatchMixInfo () {
-            const result = this.getMixInfo()
-
-            this.title = result.title
-            this.desc = result.desc
-            this.summary = result.summary
-        }
-    },
-    mounted () {
-        this.dispatchMixInfo()
+  components: { Spinner },
+  data() {
+    return {
+      loading: true,
+      error: false,
+      id: parseInt(this.$route.params.id),
+      item: null,
     }
+  },
+  methods: {
+    getMixInfo(id) {
+      const found = this.$store.state.api.mix.find((item) => item.id === id)
+
+      return found
+    },
+  },
+  mounted() {
+    this.loading = true
+    const found = this.getMixInfo(this.id)
+
+    if (found) this.item = { ...found }
+    else this.error = { message: '믹스를 찾을 수 없습니다.' }
+
+    this.loading = false
+  },
 }
 </script>
