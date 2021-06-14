6d67f790c1312580344989fefae6734ed84afbf1
devDohyun <dohyun10101@gmail.com>
2021-06-15 06:19:32 +0900

    removed nuxt key

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,43 @@
 <template>
-    <div id="app">
-        <default-navigation></default-navigation>
-        <transition name="fade" mode="out-in">
-            <nuxt :key="$route.fullPath" />
-        </transition>
-        <default-footer></default-footer>
-    </div>
+  <div id="app">
+    <default-navigation></default-navigation>
+    <transition name="fade" mode="out-in">
+      <nuxt />
+    </transition>
+    <default-footer></default-footer>
+  </div>
 </template>
 
 <style lang="scss">
-    * {
-        box-sizing: border-box;
-    }
-    html, body {
-        min-height: 100%;
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  min-height: 100%;
 
-        margin: 0;
-        padding: 0;
-    }
-    html {
-        overflow-y: auto;
-    }
-    #app {
-        height: 100%;
-        min-height: 100vh;
-        background-color: black;
-        color: white;
-        font-family: Roboto, Noto Naskh Arabic UI, Arial, sans-serif;
-    }
+  margin: 0;
+  padding: 0;
+}
+html {
+  overflow-y: auto;
+}
+#app {
+  height: 100%;
+  min-height: 100vh;
+  background-color: black;
+  color: white;
+  font-family: Roboto, Noto Naskh Arabic UI, Arial, sans-serif;
+}
 
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity 0.2s ease;
-    }
-    .fade-enter, .fade-leave-active {
-        opacity: 0;
-    }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 </style>
 
 <script>
@@ -42,9 +45,9 @@ import DefaultNavigation from '@/components/Navigation'
 import DefaultFooter from '@/components/Footer'
 
 export default {
-    components: {
-        DefaultNavigation,
-        DefaultFooter
-    },
+  components: {
+    DefaultNavigation,
+    DefaultFooter,
+  },
 }
 </script>
